ae3b04374889ea98d37b8fb45987b1e36214b1dd
hasbiardianto <rdnthasbi@gmail.com>
2024-03-16 16:58:10 +0700

    Add tailwindcss config and flowbite ui

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Kontak Kami</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
